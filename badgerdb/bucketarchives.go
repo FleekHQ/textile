75ea1e5bf8b11a86ffeefe6808fcf0c36d3645c5
jsonsivar <jsonsivar@gmail.com>
2020-11-12 00:20:52 -0500

    Optional-mongo: implement bucket archives for badger

--- badgerdb/bucketarchives.go
+++ badgerdb/bucketarchives.go
@@ -2,8 +2,10 @@ package badgerdb
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/FleekHQ/space-daemon/core/store"
+	"github.com/dgraph-io/badger"
 	"github.com/textileio/textile/v2/model"
 )
 
@@ -11,19 +13,68 @@ type BucketArchives struct {
 	st store.Store
 }
 
+const bucketKeyStoreKeyPrefix = "bucketarchives_"
+
 func NewBucketArchives(_ context.Context, st store.Store) (*BucketArchives, error) {
 	b := &BucketArchives{st: st}
 	return b, nil
 }
 
 func (b *BucketArchives) Create(ctx context.Context, bucketKey string) (*model.BucketArchive, error) {
-	return nil, errNotImplemented
+	ba := &model.BucketArchive{
+		BucketKey: bucketKey,
+	}
+
+	data, err := json.Marshal(*ba)
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.st.Set([]byte(bucketKeyStoreKeyPrefix+bucketKey), data)
+	if err != nil {
+		return nil, err
+	}
+
+	return ba, err
 }
 
 func (b *BucketArchives) Replace(ctx context.Context, ba *model.BucketArchive) error {
-	return errNotImplemented
+	_, err := b.st.Get([]byte(bucketKeyStoreKeyPrefix + ba.BucketKey))
+	if err != nil {
+		return err
+	}
+
+	// we dont do anything with res from  above
+	// it was just to return an error if not found
+
+	data, err := json.Marshal(*ba)
+	if err != nil {
+		return err
+	}
+
+	err = b.st.Set([]byte(bucketKeyStoreKeyPrefix+ba.BucketKey), data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (b *BucketArchives) GetOrCreate(ctx context.Context, bucketKey string) (*model.BucketArchive, error) {
-	return nil, errNotImplemented
+	var ba *model.BucketArchive
+
+	raw, err := b.st.Get([]byte(bucketKeyStoreKeyPrefix + bucketKey))
+	if err != nil {
+		if err == badger.ErrKeyNotFound {
+			return b.Create(ctx, bucketKey)
+		}
+		return nil, err
+	}
+
+	err = json.Unmarshal(raw, ba)
+	if err != nil {
+		return nil, err
+	}
+
+	return ba, nil
 }
