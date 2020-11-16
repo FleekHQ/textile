29615f85b56587a6eb70d60da12b9ec9a48d1de7
jsonsivar <jsonsivar@gmail.com>
2020-11-16 11:57:50 -0500

    Optional-mongo: add placeholders for invites since we use a general version of that in gateway

--- badgerdb/invites.go
+++ badgerdb/invites.go
@@ -4,7 +4,8 @@ import (
 	"context"
 
 	"github.com/FleekHQ/space-daemon/core/store"
-	"github.com/FleekHQ/textile/v2/model"
+	"github.com/textileio/go-threads/core/thread"
+	"github.com/textileio/textile/v2/model"
 )
 
 type Invites struct {
@@ -16,8 +17,16 @@ func NewInvites(ctx context.Context, st store.Store) (*Invites, error) {
 	return k, nil
 }
 
+func (i *Invites) Create(ctx context.Context, from thread.PubKey, org, emailTo string) (*model.Invite, error) {
+	return nil, errNotImplemented
+}
+
 func (i *Invites) Get(ctx context.Context, token string) (*model.Invite, error) {
 	return nil, errNotImplemented
+}
+
+func (i *Invites) ListByEmail(ctx context.Context, email string) ([]model.Invite, error) {
+	return []model.Invite{}, errNotImplemented
 }
 
 func (i *Invites) Accept(ctx context.Context, token string) error {
@@ -25,5 +34,17 @@ func (i *Invites) Accept(ctx context.Context, token string) error {
 }
 
 func (i *Invites) Delete(ctx context.Context, token string) error {
+	return errNotImplemented
+}
+
+func (i *Invites) DeleteByFrom(ctx context.Context, from thread.PubKey) error {
+	return errNotImplemented
+}
+
+func (i *Invites) DeleteByOrg(ctx context.Context, org string) error {
+	return errNotImplemented
+}
+
+func (i *Invites) DeleteByFromAndOrg(ctx context.Context, from thread.PubKey, org string) error {
 	return errNotImplemented
 }
--- collections/invites.go
+++ collections/invites.go
@@ -3,6 +3,7 @@ package collections
 import (
 	"context"
 
+	"github.com/textileio/go-threads/core/thread"
 	"github.com/textileio/textile/v2/badgerdb"
 	"github.com/textileio/textile/v2/model"
 	"github.com/textileio/textile/v2/mongodb"
@@ -40,14 +41,66 @@ func NewInvites(_ context.Context, hub bool, opts ...InvitesOptions) (*Invites, 
 	return k, nil
 }
 
+func (i *Invites) Create(ctx context.Context, from thread.PubKey, org, emailTo string) (*model.Invite, error) {
+	if i.hub {
+		return i.m.Create(ctx, from, org, emailTo)
+	} else {
+		return i.b.Create(ctx, from, org, emailTo)
+	}
+}
+
 func (i *Invites) Get(ctx context.Context, token string) (*model.Invite, error) {
-	return nil, errNotImplemented
+	if i.hub {
+		return i.m.Get(ctx, token)
+	} else {
+		return i.b.Get(ctx, token)
+	}
+}
+
+func (i *Invites) ListByEmail(ctx context.Context, email string) ([]model.Invite, error) {
+	if i.hub {
+		return i.m.ListByEmail(ctx, email)
+	} else {
+		return i.b.ListByEmail(ctx, email)
+	}
 }
 
 func (i *Invites) Accept(ctx context.Context, token string) error {
-	return errNotImplemented
+	if i.hub {
+		return i.m.Accept(ctx, token)
+	} else {
+		return i.b.Accept(ctx, token)
+	}
 }
 
 func (i *Invites) Delete(ctx context.Context, token string) error {
-	return errNotImplemented
+	if i.hub {
+		return i.m.Delete(ctx, token)
+	} else {
+		return i.b.Delete(ctx, token)
+	}
+}
+
+func (i *Invites) DeleteByFrom(ctx context.Context, from thread.PubKey) error {
+	if i.hub {
+		return i.m.DeleteByFrom(ctx, from)
+	} else {
+		return i.b.DeleteByFrom(ctx, from)
+	}
+}
+
+func (i *Invites) DeleteByOrg(ctx context.Context, org string) error {
+	if i.hub {
+		return i.m.DeleteByOrg(ctx, org)
+	} else {
+		return i.b.DeleteByOrg(ctx, org)
+	}
+}
+
+func (i *Invites) DeleteByFromAndOrg(ctx context.Context, from thread.PubKey, org string) error {
+	if i.hub {
+		return i.m.DeleteByFromAndOrg(ctx, from, org)
+	} else {
+		return i.b.DeleteByFromAndOrg(ctx, from, org)
+	}
 }
